bdb476ed8cb57cd0a71f89eb0269333774dc7afe
ramonfean <ramonfean@gmail.com>
2025-03-29 19:07:13 +0100

    a

--- Pandas.py
+++ Pandas.py
@@ -1,42 +1,7 @@
-from Paciente import *
-from Colas import *
-from Gestor import *
-import pandas
-
-
-cola_admision = Cola()
-cola_general_urgente = Cola()
-cola_general_no_urgente = Cola()
-cola_especialidad_urgente = Cola()
-cola_especialidad_no_urgente = Cola()
-consulta_general = Consulta()
-consulta_especialidad = Consulta()
-prioridad_activa = []
-
-
-with open(r"C:\Users\Usuario\Desktop\programacion2\p2\patients0.txt", "r", encoding="utf-8") as file:
-    for linea in file:
-        datos = linea.strip().split(" ")
-        IDPAC = str(datos[0])
-        tipo_consulta = datos[1]
-        urgencia = datos[2]
-        tiempo_estimado = int(datos[3])
-
-        paciente = Patient(IDPAC, tipo_consulta, urgencia, tiempo_estimado)
-        cola_admision.enqueue(paciente)
-
-gestor_turnos = Gestor_Turnos(cola_admision, consulta_general, consulta_especialidad, cola_general_urgente, cola_general_no_urgente, cola_especialidad_urgente, cola_especialidad_no_urgente, prioridad_activa, 0)
-while not cola_admision.is_empty() or not consulta_general.is_empty() or not consulta_especialidad.is_empty():    
-    gestor_turnos.avanzar_tiempo()
-    if (gestor_turnos._tiempo -1) % 3 == 0:
-        if cola_admision:
-            gestor_turnos.asignar_cola(cola_admision.dequeue())
-    gestor_turnos.asignar_consulta()
-    
-    
-
-    
-
+import pandas 
+import Colas
+import Paciente
+import Gestor
 
 
 def generar_estadisticas(datos_pacientes):
@@ -110,5 +75,3 @@ generar_estadisticas(datos_pacientes)
 
 # # Generate statistics from collected data
 # generar_estadisticas(gestor_turnos._finished_patients)
-
-    
